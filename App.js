609fabc1728d62b9e9dce2f45b588faa60a572d9
Rhouma Mohamed <medrhouma1234@gmail.com>
2025-10-25 10:47:53 +0100

    Add loading indicator while app is not ready

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ export default function App() {
 
   if (!isReady) {
     return (
+      
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator size="large" color="#007AFF" />
       </View>
